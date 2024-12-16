23697b07a207bc9f077f11a008c4a02c9c443c82
Anastasia <karpovanastiya@mail.ru>
2024-12-16 23:16:14 +0300

    background and foreground color

--- Clock/MainForm.cs
+++ Clock/MainForm.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Text;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,42 +13,152 @@ namespace Clock
 {
 	public partial class MainForm : Form
 	{
+		ColorDialog BackColorDialog;
+		ColorDialog ForeColorDialog;
+		PrivateFontCollection font;
 		public MainForm()
 		{
 			InitializeComponent();
+			//fontsProjects();
+			//fonts();
+
+			labelTime.BackColor = Color.AliceBlue;
+
+			BackColorDialog = new ColorDialog();
+			ForeColorDialog = new ColorDialog();
+			this.Location = new Point(Screen.PrimaryScreen.Bounds.Width - this.Width, 50);
+			
+		}
+
+		void SetVisibility(bool visible)
+		{
+			cbShowDate.Visible = visible;
+			cbShowWeekDay.Visible = visible;
+			cmBackColor.Visible = visible;
+			cmForeColor.Visible = visible;
+			btnHideControls.Visible = visible;
+			this.TransparencyKey = visible ? Color.Empty : this.BackColor;
+			this.FormBorderStyle = visible ? FormBorderStyle.FixedToolWindow : FormBorderStyle.None;
+			this.ShowInTaskbar = visible;
+			//fontsProjects();
+			//fonts();
 		}
 
 		private void timer_Tick(object sender, EventArgs e)
 		{
 			labelTime.Text = DateTime.Now.ToString("hh:mm:ss tt", System.Globalization.CultureInfo.InvariantCulture);
-			if(cbShowDate.Checked )
+			if(cbShowDate.Checked)
 			{
 				labelTime.Text += "\n";
 				labelTime.Text += DateTime.Now.ToString("yyyy.MM.dd");
 			}
+			if(cbShowWeekDay.Checked)
+			{
+				labelTime.Text += "\n";
+				labelTime.Text += DateTime.Now.DayOfWeek;
+			}
+			notifyIcon.Text = "";
+			notifyIcon.Text = labelTime.Text;
+
 		}
 
 		private void btnHideControls_Click(object sender, EventArgs e)
 		{
-			cbShowDate.Visible = false;
-			btnHideControls.Visible = false;
-			this.TransparencyKey = this.BackColor;
-			this.FormBorderStyle = FormBorderStyle.None;
-			labelTime.BackColor = Color.AliceBlue;
-			this.ShowInTaskbar = false;
+			//cbShowDate.Visible = false;
+			//btnHideControls.Visible = false;
+			//this.TransparencyKey = this.BackColor;
+			//this.FormBorderStyle = FormBorderStyle.None;
+			//labelTime.BackColor = Color.AliceBlue;
+			//this.ShowInTaskbar = false;
 			//Color transparencyKey = this.TransparencyKey;
+
+			SetVisibility(false);	
 		}
 
 		private void labelTime_DoubleClick(object sender, EventArgs e)
 		{
-			MessageBox.Show
-				(
-				this, 
-				"Вы два раза щелкнули мышью по времени, и теперь Вы управляете временем",
-				"Info", 
-				MessageBoxButtons.OK,
-				MessageBoxIcon.Information
-				);
+			//MessageBox.Show
+			//	(
+			//	this, 
+			//	"Вы два раза щелкнули мышью по времени, и теперь Вы управляете временем",
+			//	"Info", 
+			//	MessageBoxButtons.OK,
+			//	MessageBoxIcon.Information
+			//	);
+
+			//cbShowDate.Visible = true;
+			//btnHideControls.Visible = true;
+			//this.TransparencyKey = Color.Empty;
+			//this.FormBorderStyle = FormBorderStyle.FixedToolWindow;
+			//labelTime.BackColor = Color.AliceBlue;
+			//this.ShowInTaskbar = true;
+
+			SetVisibility(true);
+		}
+
+		private void cmExit_Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+
+		private void cmTopmost_CheckedChanged(object sender, EventArgs e)
+		{
+			this.TopMost = cmTopmost.Checked;
+		}
+
+		private void cmShowDate_CheckedChanged(object sender, EventArgs e)
+		{
+			cbShowDate.Checked = cmShowDate.Checked;
+		}
+		private void cbShowDate_CheckedChanged(object sender, EventArgs e)
+		{
+			cmShowDate.Checked = cbShowDate.Checked;
+		}
+
+		private void cmShowWeekDay_CheckedChanged(object sender, EventArgs e)
+		{
+			cbShowWeekDay.Checked = cmShowWeekDay.Checked;
+		}
+		private void cbShowWeekDay_CheckedChanged(object sender, EventArgs e)
+		{
+			cmShowWeekDay.Checked = cbShowWeekDay.Checked;
+		}
+
+		private void notifyIcon_DoubleClick(object sender, EventArgs e)
+		{
+			if(!this.TopMost)
+			{
+				this.TopMost = true;
+				this.TopMost = false;
+			}
+		}
+
+		private void cmBackColor_Click(object sender, EventArgs e)
+		{
+			DialogResult result = BackColorDialog.ShowDialog(this);
+			if(result == DialogResult.OK)
+			{
+				labelTime.BackColor = BackColorDialog.Color;
+			}
+		}
+
+		private void cmForeColor_Click(object sender, EventArgs e)
+		{
+			DialogResult result = ForeColorDialog.ShowDialog(this);
+			if (result == DialogResult.OK)
+			{
+				labelTime.ForeColor = ForeColorDialog.Color;
+			}
+		}
+
+		private void fontsProjects()
+		{
+			this.font = new PrivateFontCollection();
+			this.font.AddFontFile("Font/5by7.ttf");
+		}
+		private void fonts()
+		{
+			labelTime.Font = new Font(font.Families[0], 31);
 		}
 	}
 }
